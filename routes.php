3aea4da158fff07b5d9d5da3515e3343c2add820
Rik Janssen <56626554+rik-janssen@users.noreply.github.com>
2023-01-20 18:29:07 +0100

    1

--- routes.php
+++ routes.php
@@ -3,8 +3,15 @@
 use Route\Route;
 use Middleware\Test;
 
+/**
+ * These routes will always overwrite 
+ * the routes in the database on call.
+ */
 
-Route::register("user/edit/1", "UserController@edit")
+Route::register("/home", "HomeController@index")
+        ->store("get");
+
+ Route::register("user/edit/1", "UserController@edit")
         ->auth('true|false')
         ->role('admin|user|array?')
         ->armour("Test@stop", "value(optional)")
@@ -18,7 +25,7 @@ Route::register("user/edit/1", "UserController@edit")
         ->armour("Test@stop", "value(optional)")
         ->limit(86400, 1000, 'groupname')
         ->model("User",1)
-        ->store("get");
+        ->store("post");
 
         Route::register("user/edit/3", "UserController@edit")
         ->auth('true|false')
@@ -38,3 +45,5 @@ Route::register("user/edit/1", "UserController@edit")
         // limit: time in seconds, the amount of calls within that time (from first call), the limit group so you can do pages indiviually ore group them together by name
         // model: model and id
         // store: dump everything in the database and also set the method of retrieval: get|post|delete|update
+
+        // when changing the register('/path') a new route will be registered.
