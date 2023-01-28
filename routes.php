189ab9d2fdef4c61b3ad68b6b1fa4402bb38f771
Rik Janssen <56626554+rik-janssen@users.noreply.github.com>
2023-01-28 21:47:00 +0100

    routing / middleware

--- routes.php
+++ routes.php
@@ -8,37 +8,47 @@ use Middleware\Test;
  * the routes in the database on call.
  */
 
-Route::register("/home", "HomeController@index")
+Route::register("/", "HomeController@index")
+        ->redirect('{url}/somewhere')
         ->store("get");
 
- Route::register("user/edit/1", "UserController@edit")
-        ->auth('true|false')
+Route::register("user/edit/1", "UserController@edit")
+        ->auth('true')
         ->role('admin|user|array?')
-        ->armour("Test@stop", "value(optional)")
+        ->armour("Test@run", "value(optional)")
         ->limit(86400, 1000, 'groupname')
         ->model("User",1)
         ->store("get");
 
-        Route::register("user/edit/2", "UserController@edit")
-        ->auth('true|false')
+Route::register("user/edit/1/save", "UserController@edit")
+        ->auth('true')
         ->role('admin|user|array?')
         ->armour("Test@stop", "value(optional)")
         ->limit(86400, 1000, 'groupname')
         ->model("User",1)
         ->store("post");
 
-        Route::register("user/edit/3", "UserController@edit")
-        ->auth('true|false')
+Route::register("user/edit/2", "UserController@edit")
+        ->auth('true')
+        ->role(['admin', 'moderator', 'user_self'])
+        ->armour("Test@stop", "value(optional)")
+        ->limit(86400, 1000, 'groupname')
+        ->model("User",2)
+        ->store("post");
+
+Route::register("user/edit/hank_the_incredible", "UserController@edit")
+        ->auth('true')
         ->role('admin|user|array?')
         ->armour("Test@stop", "value(optional)")
         ->limit(86400, 1000, 'groupname')
-        ->model("User",1)
+        ->model("User",3)
         ->store("get");
 
         // the order of the middleware stack is of importance
         // this case will block users without a session first
         // if there is a session, check the role.. etc..
 
+        // redirect: can redirect a page to another url or internally by using {url}/to_this_page tag as the placeholder
         // auth: true, needs session | false, needs no session
         // role: role slug
         // armour: a custom class that can do whatever it wants, die, redirect...
